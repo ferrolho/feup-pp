9f03a51eef99f7b02109adc70f3f2d5a58ad822e
ferrolho <henriqueferrolho@gmail.com>
2016-03-21 13:34:27 +0000

    Finished first part of the project. Output files for each tissue are now working

--- project/parser.cpp
+++ project/parser.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <memory>
 #include <vector>
 #include <sstream>
 using namespace std;
@@ -11,7 +12,6 @@ struct gtexAnalysisFile_t {
 	unsigned int genesNum, samplesNum;
 
 	vector<string> genesHeader;
-	vector<vector<string>> genesData;
 };
 
 void tokenize(const string& str, vector<string>& tokens, const string& delimiters = " ") {
@@ -49,13 +49,53 @@ vector<string> splitTSV(const string& str) {
 	return tokens;
 }
 
+multimap<string, string> buildTissueToSamplesMap(const string& samplesFilename, const gtexAnalysisFile_t& gtexAnalysisFile) {
+	ifstream gtexDataIn;
+	gtexDataIn.open(samplesFilename);
+
+	cout << endl << "> Reading samples file ... " << flush;
+
+	string line;
+	getline(gtexDataIn, line);
+
+	multimap<string, string> tissueSamplesMap;
+
+	for (unsigned int i = 0; i < gtexAnalysisFile.samplesNum; i++) {
+		getline(gtexDataIn, line);
+
+		vector<string> tokens = splitTSV(line);
+
+		tissueSamplesMap.insert(pair<string, string>(tokens[6], tokens[0]));
+	}
+
+	gtexDataIn.close();
+
+	// ignore tissues with less than 10 samples
+	for (auto it = tissueSamplesMap.cbegin(); it != tissueSamplesMap.cend();) {
+		if (tissueSamplesMap.count(it->first) < 10) {
+			auto nextIt = tissueSamplesMap.upper_bound(it->first);
+
+			tissueSamplesMap.erase(it->first);
+
+			it = nextIt;
+		} else {
+			it = tissueSamplesMap.upper_bound(it->first);
+		}
+	}
+
+	//for (auto it = tissueSamplesMap.begin(), end = tissueSamplesMap.end(); it != end; it = tissueSamplesMap.upper_bound(it->first))
+	//	cout << it->first << " -> " << tissueSamplesMap.count(it->first) << endl;
+
+	cout << "OK!" << endl << endl;
+
+	return tissueSamplesMap;
+}
+
 int main(int argc, char* argv[]) {
 	if (argc < 3) {
 		cerr << "Error: Too few arguments" << endl;
 		return 1;
 	}
-
-	string line;
 
 	/*
 	* Read genes file header (the ~5gb .txt file)
@@ -74,62 +114,25 @@ int main(int argc, char* argv[]) {
 	cout << "No. samples: " << gtexAnalysisFile.samplesNum << endl;
 	cout << "-----------------" << endl;
 
-	cout << "> Reading giant genes file (~5gb takes time, go grab a snack) ... " << flush;
+	string line;
 
 	getline(gtexAnalysisIn, line);
 	gtexAnalysisFile.genesHeader = splitTSV(line);
 
-	for (unsigned int i = 0; i < gtexAnalysisFile.genesNum; i++) {
-		getline(gtexAnalysisIn, line);
-		gtexAnalysisFile.genesData.push_back(splitTSV(line));
-	}
-
-	gtexAnalysisIn.close();
-	cout << "OK!" << endl;	
-
 	// this map makes it possible to return the column of a certain sample in O(1)
 	map<string, unsigned int> sampleColumnMap;
-
 	for (unsigned int i = 0; i < gtexAnalysisFile.genesHeader.size(); i++)
 		sampleColumnMap[gtexAnalysisFile.genesHeader[i]] = i;
+
 
 	/*
 	* Build [tissue -> samples] multimap
 	*/
-	cout << "> Reading samples file ... " << flush;
+	multimap<string, string> tissueSamplesMap = buildTissueToSamplesMap(argv[2], gtexAnalysisFile);
 
-	ifstream gtexDataIn;
-	gtexDataIn.open(argv[2]);
-	getline(gtexDataIn, line);
 
-	multimap<string,string> tissueSamplesMap;
-
-	for (unsigned int i = 0; i < gtexAnalysisFile.samplesNum; i++) {
-		getline(gtexDataIn, line);
-
-		vector<string> tokens = splitTSV(line);
-
-		tissueSamplesMap.insert(pair<string, string>(tokens[6], tokens[0]));
-	}
-
-	gtexDataIn.close();
-	cout << "OK!" << endl;
-
-	// ignore tissues with less than 10 samples
-	for (auto it = tissueSamplesMap.cbegin(); it != tissueSamplesMap.cend();) {
-		if (tissueSamplesMap.count(it->first) < 10) {
-			auto nextIt = tissueSamplesMap.upper_bound(it->first);
-
-			tissueSamplesMap.erase(it->first);
-
-			it = nextIt;
-		} else {
-			it = tissueSamplesMap.upper_bound(it->first);
-		}
-	}
-
-	//for (auto it = tissueSamplesMap.begin(), end = tissueSamplesMap.end(); it != end; it = tissueSamplesMap.upper_bound(it->first))
-	//	cout << it->first << " -> " << tissueSamplesMap.count(it->first) << endl;
+	// maps each tissue name to its file output stream
+	map<string, shared_ptr<ofstream>> tissuesFilesMap;
 
 	/*
 	* Output genes file for each tissue
@@ -137,42 +140,65 @@ int main(int argc, char* argv[]) {
 	for (auto it = tissueSamplesMap.begin(), end = tissueSamplesMap.end(); it != end; it = tissueSamplesMap.upper_bound(it->first)) {
 		string tissueName = it->first;
 
-		cout << "> Generating " << tissueName << ".txt ... " << flush;
+		cout << "> Opening " << tissueName << ".txt ... " << flush;
 
-		ofstream fout;
-		fout.open("gtex/rna-seq-data/tissues-output/" + tissueName + ".txt");
+		string filename = "gtex/rna-seq-data/tissues-output/" + tissueName + ".txt";
+		tissuesFilesMap[tissueName] = make_shared<ofstream> (filename);
 
-		fout << gtexAnalysisFile.header << endl;
-		fout << gtexAnalysisFile.genesNum << "\t" << tissueSamplesMap.count(tissueName) << endl;
+		*tissuesFilesMap[tissueName] << gtexAnalysisFile.header << endl;
+		*tissuesFilesMap[tissueName] << gtexAnalysisFile.genesNum << "\t" << tissueSamplesMap.count(tissueName) << endl;
 
 		// Name and Description columns headers
 		for (unsigned int i = 0; i < 2; i++)
-			fout << gtexAnalysisFile.genesHeader[i] << "\t";
+			*tissuesFilesMap[tissueName] << gtexAnalysisFile.genesHeader[i] << "\t";
 
 		auto samplesRange = tissueSamplesMap.equal_range(tissueName);
 		for (auto it = samplesRange.first; it != samplesRange.second;) {
-			fout << it->second;
+			*tissuesFilesMap[tissueName] << it->second;
 
-			fout << (++it != samplesRange.second ? "\t" : "\n");
+			*tissuesFilesMap[tissueName] << (++it != samplesRange.second ? "\t" : "\n");
 		}
-
-		for (auto geneData : gtexAnalysisFile.genesData) {
-			// Name and Description columns data
-			for (unsigned int i = 0; i < 2; i++)
-				fout << geneData[i] << "\t";
-
-			for (auto it = samplesRange.first; it != samplesRange.second;) {
-				// some samples are not present in the giant genes file; they are marked as N/A in the output files
-				fout << (sampleColumnMap[it->second] < 2 ? "N/A" : geneData[sampleColumnMap[it->second]]);
-
-				fout << (++it != samplesRange.second ? "\t" : "\n");
-			}
-		}
-
-		fout.close();
 
 		cout << "OK!" << endl;
 	}
+
+	cout << endl << "> Reading giant genes file (~5gb takes time, go grab a snack) ..." << endl;
+
+	for (unsigned int i = 0; i < gtexAnalysisFile.genesNum; i++) {
+		getline(gtexAnalysisIn, line);
+		vector<string> geneData = splitTSV(line);
+
+		for (auto pair : tissuesFilesMap) {
+			string tissueName = pair.first;
+
+			// Name and Description columns data
+			for (unsigned int i = 0; i < 2; i++)
+				*tissuesFilesMap[tissueName] << geneData[i] << "\t";
+
+			auto samplesRange = tissueSamplesMap.equal_range(tissueName);
+			for (auto it = samplesRange.first; it != samplesRange.second;) {
+				// some samples are not present in the giant genes file; they are marked as N/A in the output files
+				*tissuesFilesMap[tissueName] << (sampleColumnMap[it->second] < 2 ? "N/A" : geneData[sampleColumnMap[it->second]]);
+
+				*tissuesFilesMap[tissueName] << (++it != samplesRange.second ? "\t" : "\n");
+			}
+		}
+
+		printf("\r%5.1f %%", (i + 1) * 100.0 / gtexAnalysisFile.genesNum);
+	}
+
+	gtexAnalysisIn.close();
+	cout << " OK!" << endl << endl;
+
+	for (auto pair : tissuesFilesMap) {
+		cout << "> Closing " << pair.first << ".txt ... " << flush;
+
+		pair.second->close();
+
+		cout << "OK!" << endl;
+	}
+
+	cout << endl;
 
 	return 0;
 }
