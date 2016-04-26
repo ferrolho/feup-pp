1419a4d455bffe729b736858426e1986615f981a
ferrolho <henriqueferrolho@gmail.com>
2016-04-26 17:36:06 +0100

    Adds template

---
+++ coexpr/resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	@include('_head')
+
+	<body>
+
+		@include('_navbar')
+
+		@yield('content')
+
+		@include('_footer')
+
+		@include('_scripts')
+
+	</body>
+
+</html>
