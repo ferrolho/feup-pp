a84ceb8714a8efaacb23b8b529e1424542da725a
ferrolho <henriqueferrolho@gmail.com>
2016-05-12 18:02:19 +0100

    Fixes navbar links

--- coexpr/resources/views/_navbar.blade.php
+++ coexpr/resources/views/_navbar.blade.php
@@ -14,9 +14,9 @@
 
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#home">Home</a></li>
-				<li><a href="#team">Team</a></li>
-				<li><a href="#contact">Contact</a></li>
+				<li><a href="/#home">Home</a></li>
+				<li><a href="/#team">Team</a></li>
+				<li><a href="/#contact">Contact</a></li>
 			</ul>
 		</div>
 	</div>
