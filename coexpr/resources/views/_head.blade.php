b225d0bac84cb3f0d2fa1657a85668f9eb9c3399
ferrolho <henriqueferrolho@gmail.com>
2016-05-17 01:01:37 +0100

    Fixes links - for real

--- coexpr/resources/views/_head.blade.php
+++ coexpr/resources/views/_head.blade.php
@@ -14,25 +14,25 @@
 	<meta property="og:url" content="">
 	<meta property="og:image" content="">
 
-	<link href="css/app.css" rel="stylesheet">
+	<link href="{{ url('css/app.css') }}" rel="stylesheet">
 
 	<!-- Bootstrap Core CSS -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
 
 	<!-- bootstrap-slider.js -->
 	<link href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/7.0.2/css/bootstrap-slider.min.css" rel="stylesheet">
 
 	<!-- Template CSS -->
-	<link href="css/landing-page.css" rel="stylesheet" type="text/css">
+	<link href="{{ url('css/landing-page.css') }}" rel="stylesheet" type="text/css">
 
 	<!-- CoExpr -->
-	<link href="css/coexpr/coexpr.css" rel="stylesheet">
-	<link href="css/coexpr/loading.css" rel="stylesheet">
+	<link href="{{ url('css/coexpr/coexpr.css') }}" rel="stylesheet">
+	<link href="{{ url('css/coexpr/loading.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic"
 		  rel="stylesheet" type="text/css">
-	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="{{ url('font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
